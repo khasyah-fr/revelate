6b104d4e21f5ad6d0857a3f0cc1c9bb722794d29
fitra.khasyah <khasyahfr@gmail.com>
2025-03-07 20:01:29 +0700

    feat: crud news mongodb

--- src/app.js
+++ src/app.js
@@ -18,6 +18,61 @@ mongoose.connect(process.env.MONGO_URI, {
     console.error('MongoDb connection error: ', err)
 })
 
+app.post('/news', async (req, res) => {
+    try {
+        const news = new News(req.body)
+        await news.save()
+        res.status(201).json(news)
+    } catch (error) {
+        res.status(400).json({error: error.message})        
+    }
+})
+
+app.get('/news', async (req, res) => {
+    try {
+        const news = await News.find()
+        res.status(200).json(news)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.get('/news/:id', async (req, res) => {
+    try {
+        const news = await News.findById(req.params.id)
+        if (!news) {
+            return res.status(404).json({message: 'News not found'})
+        }
+        return res.status(200).json(news)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.put('/news/:id', async (req, res) => {
+    try {
+        const news = await News.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+        if (!news) {
+            return res.status(404).json({message: 'News not found'})
+        }
+        res.status(200).json(news)
+    } catch (error) {
+        res.status(400).json({error: error.message})        
+    }
+})
+
+app.delete('/news/:id', async (req, res) => {
+    try {
+        const news = await News.findByIdAndDelete(req.params.id)
+        if (!news) {
+            return res.status(404).json({messages: 'News not found'})
+        }
+        res.status(200).json({message: 'News deleted'})
+    } catch (error) {
+        res.status(500).json({error: error.message})        
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
